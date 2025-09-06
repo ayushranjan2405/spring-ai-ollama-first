f3a57270a1b6207848097d272d0b482b1c54fdc3
ayushranjan2405 <ayushranjan2405@gmail.com>
2025-09-06 21:35:23 +0530

    Initial Commit

---
+++ src/main/java/com/spring/ollama/SpringAiOllamaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOllamaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOllamaFirstApplication.class, args);
+	}
+
+}
